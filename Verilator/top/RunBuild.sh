1bcd72367cf4c0f9683427867e5580db8677206e
Roger Pease <rogerpease@gmail.com>
2022-01-21 18:25:56 -0600

    Changed to SystemVerilog

--- Verilator/top/RunBuild.sh
+++ Verilator/top/RunBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
 set -e
-verilator ../../Verilog/module/AXISimpleStreamFIFO.v --cc --exe TestModule.cpp ../../cpp/AXIMasterStreamBFM.cpp ../../cpp/AXISlaveStreamBFM.cpp  --build  --trace
+verilator ../../Verilog/module/AXISimpleStreamFIFO.sv --cc --exe TestModule.cpp ../../cpp/AXIMasterStreamBFM.cpp ../../cpp/AXISlaveStreamBFM.cpp  --build  --trace
 ./obj_dir/VAXISimpleStreamFIFO
 exit 0
